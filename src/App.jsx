3b6ec3b0b8ed6a8db3dfefee1d54875d4b33174a
Nay Thu Naing <softguide.khaingzarlynn@gmail.com>
2025-06-13 13:05:53 +0630

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import Container from "./components/Container"
-import CategorySection from "./components/CategorySection"
 import Header from "./components/Header"
-import ProductSection from "./components/ProductSection"
 import Footer from "./components/Footer"
-import { BrowserRouter } from "react-router-dom"
 function App() {
 
   return (
     <>
-      <BrowserRouter basename="/react-pos">
-        <Header />
-        <Container>
+      <Header />
+      <Container>
 
-        </Container>
-        <Footer />
-      </BrowserRouter>
+      </Container>
+      <Footer />
     </>
   )
 }
