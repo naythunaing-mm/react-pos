35cb66ed7c73fcba755b9f9d70c2b37586d82913
Nay Thu Naing <softguide.naythunaing@gmail.com>
2025-06-30 15:39:35 +0630

    fixed category btn

--- src/components/CategoryBtn.jsx
+++ src/components/CategoryBtn.jsx
@@ -7,8 +7,8 @@ const CategoryBtn = ({ id, category, current }) => {
     return (
         <>
             <button
-                onClick={handleClick} className={`${current ? "bg-black dark:text-black dark:bg-gray-600  text-white" : "bg-white text-black"
-                    } border dark:text-white border-black rounded-md p-1 px-2 hover:bg-black hover:text-white text-nowrap mx-2 dark:bg-gray-700`}
+                onClick={handleClick} className={`${current ? "bg-black dark:text-black text-white dark:bg-gray-700" : "bg-white text-black dark:text-white"
+                    } border border-black rounded-md p-1 px-2 hover:bg-black hover:text-white text-nowrap mx-2 dark:bg-gray-700`}
             >
                 {category}
             </button>
