97818938a4cd5a718513dedaf61bcc5c9af8171e
Nay Thu Naing <softguide.naythunaing@gmail.com>
2025-06-30 12:52:33 +0630

    category dakr mode btn

--- src/components/CategoryBtn.jsx
+++ src/components/CategoryBtn.jsx
@@ -7,7 +7,7 @@ const CategoryBtn = ({ id, category, current }) => {
     return (
         <>
             <button
-                onClick={handleClick} className={`${current ? "bg-black dark:bg-black  text-white" : "bg-white text-black"
+                onClick={handleClick} className={`${current ? "bg-black dark:text-black dark:bg-gray-600  text-white" : "bg-white text-black"
                     } border dark:text-white border-black rounded-md p-1 px-2 hover:bg-black hover:text-white text-nowrap mx-2 dark:bg-gray-700`}
             >
                 {category}
