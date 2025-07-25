323754de13b13f5b81fd84756977a2051b3eff12
Nay Thu Naing <softguide.naythunaing@gmail.com>
2025-07-25 12:30:57 +0630

    modify

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,10 +20,6 @@ const Cart = ({ product: { id, title, price, image, rating: { rate } } }) => {
                     <img src={image} alt={title} className="cart-img w-40 h-32 md:h-36 lg:h-36 hover:rotate-6 transition duration-700 ease-in-out" />
                 </div>
                 <h3 className="text-xs md:text-xl lg:text-xl font-semibold text-justify dark:text-gray-400">{truncate(title, 15)}</h3>
-                {/* <div className="flex items-center my-2">
-                    <Rating className="w-2" rate={rating} />
-                    <span className="ml-2 text-sm text-gray-600">({rate.toFixed(1)})</span>
-                </div> */}
                 <div className="my-2">
                     <Rating className="w-1 my-2" rate={rating} />
                 </div>
